ace854ddf2388ff5df67171c6b4e76b4e9494e63
thuykv <thuykv@fpt.com.vn>
2024-02-29 14:34:31 +0700

    fix lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5'
 import react from '@vitejs/plugin-react-swc'
 import { createRequire } from 'node:module'
-import { resolve } from 'node:path'
 import { defineConfig, loadEnv } from 'vite'
-import { name, version } from './package.json'
 import tsconfigPaths from 'vite-tsconfig-paths' // Áp dụng 'paths' trong tsconfig
 
 const require = createRequire(import.meta.url)
@@ -14,25 +12,5 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react(), ckeditor5({ theme: require.resolve('@ckeditor/ckeditor5-theme-lark') }), tsconfigPaths()],
-    build: {
-      lib: {
-        entry: resolve(__dirname, 'src/index.ts'),
-        name: 'react-base-ui',
-        fileName: 'index',
-      },
-      outDir: './lib',
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-    define: {
-      pkgJson: { name, version },
-    },
   }
 })
