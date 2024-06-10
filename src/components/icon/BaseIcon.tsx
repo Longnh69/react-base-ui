ba8a3b8eba90c22cc5082f21a1e721487e39204b
thuykv <thuykv@fpt.com.vn>
2024-06-10 11:15:42 +0700

    not add new icons to base folder, add to custom folder instead of

---
+++ src/components/icon/BaseIcon.tsx
@@ -0,0 +1,11 @@
+import Icon from '@ant-design/icons'
+import { type CustomIconComponentProps } from '@ant-design/icons/lib/components/Icon'
+import { twMerge } from 'tailwind-merge'
+
+export interface BaseIconProps extends Partial<CustomIconComponentProps> {}
+
+export default function BaseIcon(props: BaseIconProps) {
+  const { className, ...restProps } = props
+
+  return <Icon className={twMerge(`h-4 w-4`, className)} {...restProps} />
+}
