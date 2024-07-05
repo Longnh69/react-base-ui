9a21d6ecf21fe56c4bb65dd91231474036e9ac4f
thuykv <thuykv@fpt.com.vn>
2024-07-05 18:06:01 +0700

    feat(component): create file BaseCollapse.tsx

---
+++ src/components/collapse/BaseCollapse.tsx
@@ -0,0 +1,11 @@
+import { Collapse, type CollapseProps } from 'antd'
+import { forwardRef, LegacyRef } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface BaseCollapseProps extends CollapseProps {}
+
+export default forwardRef(function BaseCollapse(props: BaseCollapseProps, ref: LegacyRef<HTMLDivElement> | undefined) {
+  const { className, ...restProps } = props
+
+  return <Collapse ref={ref} className={twMerge(` `, className)} {...restProps} />
+})
