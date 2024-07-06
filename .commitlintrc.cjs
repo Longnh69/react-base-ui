24ceb52958e983f06cdb86d95162e269e61e66d3
Kieu Van Thuy (FIS DDC HCM) <thuykv@fpt.com.vn>
2024-07-06 10:16:07 +0700

    chore: add commitlint configs and setups

---
+++ .commitlintrc.cjs
@@ -0,0 +1,34 @@
+/**
+ * https://commitlint.js.org/reference/configuration.html
+ *
+ *
+ * Errors:
+ *
+ * 1. Use !
+ *  Error:  `bash:  : unrecognized history modifier`
+ *  Solution: Use single quotes instead of (" -> ')
+ *
+ *
+ */
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', Infinity],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', Infinity],
+    'header-max-length': [2, 'always', 100],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'perf', 'refactor', 'build', 'ci', 'revert', 'style', 'test', 'chore'],
+    ],
+  },
+}
