1e8b7c3fda7da11ea55076af7eb38bf7a4a89a4d
thuykv <thuykv@fpt.com.vn>
2024-08-14 19:38:30 +0700

    fix: change props cascalder

--- src/components/cascader/BaseCascader.tsx
+++ src/components/cascader/BaseCascader.tsx
@@ -1,15 +1,12 @@
 import { Cascader, type CascaderProps } from 'antd'
-import { type BaseOptionType, type CascaderRef } from 'antd/es/cascader'
-import BaseKeyboardArrowDownIcon from '../icon/BaseKeyboardArrowDownIcon'
+import { type CascaderRef } from 'antd/es/cascader'
 import { forwardRef, type Ref } from 'react'
 import { twMerge } from 'tailwind-merge'
+import BaseKeyboardArrowDownIcon from '../icon/BaseKeyboardArrowDownIcon'
 
-export type BaseCascaderProps<T extends BaseOptionType> = CascaderProps<T> & {}
+export type BaseCascaderProps = CascaderProps & {}
 
-export default forwardRef(function BaseCascader<T extends BaseOptionType>(
-  props: BaseCascaderProps<T>,
-  ref: Ref<CascaderRef> | null,
-) {
+export default forwardRef(function BaseCascader(props: BaseCascaderProps, ref: Ref<CascaderRef> | null) {
   const { className, ...restProps } = props
 
   return (
