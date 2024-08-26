c26f4f0bbb8907a78adb6afed9169272d6ee1c21
thuykv <thuykv@fpt.com.vn>
2024-08-26 17:48:01 +0700

    fix: import libs font awesome

--- src/components/font-awesome-icon/BaseFontAwesomeIcon.tsx
+++ src/components/font-awesome-icon/BaseFontAwesomeIcon.tsx
@@ -1,4 +1,10 @@
 import { FontAwesomeIcon, type FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
 
 export interface BaseFontAwesomeIconProps extends FontAwesomeIconProps {}
 
