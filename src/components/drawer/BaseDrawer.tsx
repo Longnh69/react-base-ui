86be59087551d62c128e1f59cd41630a4b6508e9
thuykv <thuykv@fpt.com.vn>
2024-09-17 17:15:29 +0700

    feat: add close position

--- src/components/drawer/BaseDrawer.tsx
+++ src/components/drawer/BaseDrawer.tsx
@@ -1,17 +1,21 @@
 import { Drawer, type DrawerProps } from 'antd'
+import _ from 'lodash'
 import { twMerge } from 'tailwind-merge'
 
-export interface BaseDrawerProps extends DrawerProps {}
+export interface BaseDrawerProps extends DrawerProps {
+  closePosition?: 'right' | 'left'
+}
 
 export default function BaseDrawer(props: BaseDrawerProps) {
-  const { className, ...restProps } = props
+  const { closePosition = 'right', className, ...restProps } = props
 
   return (
     <Drawer
       className={twMerge(
         `
-
+          
         `,
+        _.eq(closePosition, 'right') ? '[&_.ant-drawer-header-title]:flex-row-reverse' : '',
         className,
       )}
       {...restProps}
