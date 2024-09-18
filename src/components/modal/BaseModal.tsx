5c3d05b19d3b14d56b9f824f31909b022cdfed1f
thuykv <thuykv@fpt.com.vn>
2024-09-18 17:14:55 +0700

    feat: add divider to modal

--- src/components/modal/BaseModal.tsx
+++ src/components/modal/BaseModal.tsx
@@ -1,27 +1,52 @@
 import { Modal, type ModalProps } from 'antd'
 import { twMerge } from 'tailwind-merge'
+import BaseDivider from '../divider/BaseDivider'
 
-export interface BaseModalProps extends ModalProps {}
+export interface BaseModalProps extends ModalProps {
+  divider?: boolean
+}
 
 export default function BaseModal(props: BaseModalProps) {
-  const { className, rootClassName, ...restProps } = props
+  const { className, rootClassName, children, divider = true, ...restProps } = props
 
   return (
     <Modal
       className={twMerge(
         `
-
+          
         `,
+        divider ? 'p-0' : '',
         className,
       )}
       rootClassName={twMerge(
         `
-
         `,
+        divider
+          ? `
+            [&_.ant-modal-content]:p-0
+            [&_.ant-modal-content>.ant-modal-header]:px-6
+            [&_.ant-modal-content>.ant-modal-header]:pt-5
+            [&_.ant-modal-content>.ant-modal-header]:pb-2
+            [&_.ant-modal-content>.ant-modal-footer]:m-0
+            [&_.ant-modal-content>.ant-modal-footer]:px-6
+            [&_.ant-modal-content>.ant-modal-footer]:pt-4
+            [&_.ant-modal-content>.ant-modal-footer]:pb-5
+          `
+          : '',
 
         rootClassName,
       )}
       {...restProps}
-    />
+    >
+      {divider ? (
+        <div>
+          <BaseDivider className='m-0' />
+          <div className='px-6 py-3'>{children}</div>
+          <BaseDivider className='m-0' />
+        </div>
+      ) : (
+        children
+      )}
+    </Modal>
   )
 }
