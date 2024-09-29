d711abfb7d394eab00a0e182601697fbfd7d163e
thuykv <thuykv@fpt.com.vn>
2024-09-29 11:53:35 +0700

    feat: add modal confirm remove

--- src/components/modal/BaseModal.tsx
+++ src/components/modal/BaseModal.tsx
@@ -1,56 +1,75 @@
 import { Modal, type ModalProps } from 'antd'
 import { twMerge } from 'tailwind-merge'
-import BaseDivider from '../divider/BaseDivider'
 import useDynamicClassName from '../../hooks/useDynamicClassName'
 import { type PropsWithStyleCss } from '../../types/props-with-style-css.type'
+import BaseDivider from '../divider/BaseDivider'
+import BaseModalConfirmRemoveFacet from './BaseModalConfirmRemoveFacet'
 
 export interface BaseModalProps extends ModalProps, PropsWithStyleCss {
   divider?: boolean
+  facet?: 'confirm-remove' | 'default'
 }
 
 export default function BaseModal(props: BaseModalProps) {
-  const { className, rootClassName, children, divider = true, styleCss, ...restProps } = props
+  const { facet, className, rootClassName, children, divider, styleCss, ...restProps } = props
   const { dynamicClassName } = useDynamicClassName({ styleCss })
 
-  return (
-    <Modal
-      className={twMerge(
-        `
-          
-        `,
-        divider ? 'p-0' : '',
-        className,
-        dynamicClassName,
-      )}
-      rootClassName={twMerge(
-        `
-        `,
-        divider
-          ? `
-            [&_.ant-modal-content]:p-0
-            [&_.ant-modal-content>.ant-modal-header]:px-6
-            [&_.ant-modal-content>.ant-modal-header]:pt-5
-            [&_.ant-modal-content>.ant-modal-header]:pb-2
-            [&_.ant-modal-content>.ant-modal-footer]:m-0
-            [&_.ant-modal-content>.ant-modal-footer]:px-6
-            [&_.ant-modal-content>.ant-modal-footer]:pt-4
-            [&_.ant-modal-content>.ant-modal-footer]:pb-5
-          `
-          : '',
+  switch (facet) {
+    case 'confirm-remove': {
+      return (
+        <BaseModalConfirmRemoveFacet
+          className={className}
+          rootClassName={rootClassName}
+          children={children}
+          divider={divider}
+          styleCss={styleCss}
+          {...restProps}
+        />
+      )
+    }
 
-        rootClassName,
-      )}
-      {...restProps}
-    >
-      {divider ? (
-        <div>
-          <BaseDivider className='m-0' />
-          <div className='px-6 py-3'>{children}</div>
-          <BaseDivider className='m-0' />
-        </div>
-      ) : (
-        children
-      )}
-    </Modal>
-  )
+    default: {
+      return (
+        <Modal
+          className={twMerge(
+            `
+              
+            `,
+            divider ? 'p-0' : '',
+            className,
+            dynamicClassName,
+          )}
+          rootClassName={twMerge(
+            `
+            `,
+            divider
+              ? `
+                [&_.ant-modal-content]:p-0
+                [&_.ant-modal-content>.ant-modal-header]:px-6
+                [&_.ant-modal-content>.ant-modal-header]:pt-5
+                [&_.ant-modal-content>.ant-modal-header]:pb-2
+                [&_.ant-modal-content>.ant-modal-footer]:m-0
+                [&_.ant-modal-content>.ant-modal-footer]:px-6
+                [&_.ant-modal-content>.ant-modal-footer]:pt-4
+                [&_.ant-modal-content>.ant-modal-footer]:pb-5
+              `
+              : '',
+
+            rootClassName,
+          )}
+          {...restProps}
+        >
+          {divider ? (
+            <div>
+              <BaseDivider className='m-0' />
+              <div className='px-6 py-3'>{children}</div>
+              <BaseDivider className='m-0' />
+            </div>
+          ) : (
+            children
+          )}
+        </Modal>
+      )
+    }
+  }
 }
---
+++ src/components/modal/BaseModalConfirmRemoveFacet.tsx
@@ -0,0 +1,45 @@
+import { Flex } from 'antd'
+import { useTranslation } from 'react-i18next'
+import { twMerge } from 'tailwind-merge'
+import BaseButton from '../button/BaseButton'
+import BaseCircleUploadFailedIcon from '../icon/BaseCircleUploadFailedIcon'
+import BaseModal, { type BaseModalProps } from './BaseModal'
+
+export interface BaseModalConfirmRemoveFacetProps extends Omit<BaseModalProps, 'facet'> {}
+
+export default function BaseModalConfirmRemoveFacet(props: BaseModalConfirmRemoveFacetProps) {
+  const { className, title, footer, children, onCancel, onOk, ...restProps } = props
+
+  const { t } = useTranslation()
+
+  return (
+    <BaseModal
+      className={twMerge(
+        `
+          
+        `,
+        className,
+      )}
+      width={400}
+      divider={false}
+      footer={
+        <Flex className='justify-between gap-2'>
+          <BaseButton type='default' className='w-full border-none bg-light-f2f5f8' onClick={onCancel}>
+            {t('return', { defaultValue: 'Quay lại' })}
+          </BaseButton>
+          <BaseButton type='primary' danger className='w-full' onClick={onOk}>
+            {t('delete', { defaultValue: 'Xóa' })}
+          </BaseButton>
+        </Flex>
+      }
+      onCancel={onCancel}
+      onOk={onOk}
+      {...restProps}
+    >
+      <Flex className='flex-col items-center justify-center gap-4'>
+        <BaseCircleUploadFailedIcon className='h-[72px] w-[72px]' />
+        {children}
+      </Flex>
+    </BaseModal>
+  )
+}
