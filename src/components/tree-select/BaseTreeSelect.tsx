4b5d4d3c869118eca91a69c2f4401c89e34d5514
thuykv <thuykv@fpt.com.vn>
2024-10-23 10:53:59 +0700

    fix: base treeselect

--- src/components/tree-select/BaseTreeSelect.tsx
+++ src/components/tree-select/BaseTreeSelect.tsx
@@ -1,19 +1,39 @@
-import { TreeSelect, type TreeSelectProps } from 'antd'
+import { RefSelectProps, TreeSelect, type TreeSelectProps } from 'antd'
 import { twMerge } from 'tailwind-merge'
 import useDynamicClassName from '../../hooks/useDynamicClassName'
 import { type PropsWithStyleCss } from '../../types/props-with-style-css.type'
+import { forwardRef, Ref } from 'react'
+import { useTranslation } from 'react-i18next'
+import BaseKeyboardArrowDownIcon from '../icon/BaseKeyboardArrowDownIcon'
+import _ from 'lodash'
 
 export interface BaseTreeSelectProps extends TreeSelectProps, PropsWithStyleCss {}
 
-export default function BaseTreeSelect(props: BaseTreeSelectProps) {
-  const { className, styleCss, ...restProps } = props
+export default forwardRef(function BaseTreeSelect(props: BaseTreeSelectProps, ref: Ref<RefSelectProps> | null) {
+  const { className, loading, styleCss, ...restProps } = props
   const { dynamicClassName } = useDynamicClassName({ styleCss })
+  const { t } = useTranslation()
 
   return (
     <TreeSelect
+      ref={ref}
+      loading={loading}
+      placeholder={t('choose', { name: t('value') })}
       allowClear
       treeCheckable
       showCheckedStrategy={TreeSelect.SHOW_CHILD}
+      suffixIcon={!loading ? <BaseKeyboardArrowDownIcon className='pointer-events-none' /> : undefined}
+      filterTreeNode={(inputValue: string, option) => {
+        const inputValueLowerCase = inputValue.toLowerCase()
+        const { children, label, name, value } = option ?? {}
+
+        return (
+          _.includes(_.toLower(_.toString(children)), inputValueLowerCase) ||
+          _.includes(_.toLower(_.toString(name)), inputValueLowerCase) ||
+          _.includes(_.toLower(_.toString(label)), inputValueLowerCase) ||
+          _.includes(_.toLower(_.toString(value)), inputValueLowerCase)
+        )
+      }}
       className={twMerge(
         `
         `,
@@ -23,4 +43,4 @@ export default function BaseTreeSelect(props: BaseTreeSelectProps) {
       {...restProps}
     />
   )
-}
+})
