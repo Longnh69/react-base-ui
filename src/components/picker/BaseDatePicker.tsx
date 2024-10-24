547a2e718a2d28cee2d5a5a63a46d9077925551d
Kieu Van Thuy (FIS DDC HCM) <thuykv@fpt.com.vn>
2024-10-24 08:31:14 +0700

    fix: datepicker

--- src/components/picker/BaseDatePicker.tsx
+++ src/components/picker/BaseDatePicker.tsx
@@ -1,5 +1,5 @@
-import { DatePicker, type DatePickerProps } from 'antd'
-import { type PickerPanelProps } from 'rc-picker/lib/PickerPanel'
+import { DatePicker, GetProps } from 'antd'
+import { Dayjs } from 'dayjs'
 import { forwardRef, type ComponentProps, type Ref } from 'react'
 import { useTranslation } from 'react-i18next'
 import { twMerge } from 'tailwind-merge'
@@ -8,7 +8,7 @@ import { type PropsWithStyleCss } from '../../types/props-with-style-css.type'
 
 export type BaseDatePickerValue = ComponentProps<typeof DatePicker>['value']
 
-export type BaseDatePickerProps = DatePickerProps & Partial<PickerPanelProps> & PropsWithStyleCss & {}
+export type BaseDatePickerProps<ValueType = Dayjs> = GetProps<typeof DatePicker<ValueType>> & PropsWithStyleCss & {}
 
 export default forwardRef(function BaseDatePicker(props: BaseDatePickerProps, ref: Ref<any> | null) {
   const { className, styleCss, ...restProps } = props
