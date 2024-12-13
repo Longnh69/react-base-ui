7d8711229f2baeb292e311f7c99e5b8970806fa0
Kieu Van Thuy (FIS DDC HCM) <thuykv@fpt.com.vn>
2024-12-13 18:50:38 +0700

    feat: add translation support for placeholder in BaseInputNumber component

--- src/components/input/BaseInputNumber.tsx
+++ src/components/input/BaseInputNumber.tsx
@@ -3,17 +3,20 @@ import { type Ref, forwardRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 import useDynamicClassName from '../../hooks/useDynamicClassName'
 import { type PropsWithStyleCss } from '../../types/props-with-style-css.type'
+import { useTranslation } from 'react-i18next'
 
 export interface BaseInputNumberProps extends InputNumberProps, PropsWithStyleCss {}
 
 export default forwardRef(function BaseInputNumber(props: BaseInputNumberProps, ref: Ref<HTMLInputElement>) {
   const { className, styleCss, ...restProps } = props
   const { dynamicClassName } = useDynamicClassName({ styleCss })
+  const { t } = useTranslation()
 
   return (
     <InputNumber
       ref={ref}
       controls={false}
+      placeholder={t('enter', { name: t('value'), defaultValue: 'Nhập giá trị' })}
       className={twMerge(
         `
 
